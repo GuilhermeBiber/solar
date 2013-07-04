5831636ac2d9986c8502f0143d0fc621cf2dfea0
Guilherme Sampaio <guilhermebiber@gmail.com>
2013-07-03 23:37:51 -0300

    Docs

--- make.sh
+++ make.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-rm -rf bin/*
+rm -rf bin
+mkdir bin
 cd bin
 mkdir util
 g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o modelo.o ../modelo.cpp
-g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/Text.o ../util/Text.cpp
+g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/Texto.o ../util/Texto.cpp
 g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o solar.o ../solar.cpp
 g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o visao.o ../visao.cpp
 g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/explosion.o ../util/explosion.cpp
-g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/Color.o ../util/Color.cpp
 g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/geometria.o ../util/geometria.cpp
 g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o controle.o ../controle.cpp
 g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/PickRay.o ../util/PickRay.cpp
-g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/Texture.o ../util/Texture.cpp
-g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/Position.o ../util/Position.cpp
-g++ -o tpcg-gui visao.o util/geometria.o util/explosion.o util/Texture.o util/Text.o util/Position.o util/PickRay.o util/Color.o solar.o modelo.o controle.o -lm -lGL -lGLU -lglut
+g++ -DFREEGLUT_STATIC -I../util -O3 -Wall -c -fmessage-length=0 -o util/Textura.o ../util/Textura.cpp
+g++ -o solar visao.o util/geometria.o util/explosion.o util/Textura.o util/Texto.o util/PickRay.o solar.o modelo.o controle.o -lm -lGL -lGLU -lglut
